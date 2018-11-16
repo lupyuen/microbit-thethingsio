aed06792597db550fed7a0dde09b081eeafa2c00
Lee Lup Yuen <luppy@appkaki.com>
2018-11-16 13:47:54 +0800

    Update sigfox_parser.js

--- sigfox_parser.js
+++ sigfox_parser.js
@@ -1,59 +1,58 @@
 //  sigfox_parser is a thethings.io Cloud Function that is called when the
-//  Sigfox Backend delivers a sensor device message to thethings.io (via HTTP callback).
+//  Sigfox Backend delivers a sensor device message to thethings.io 
+//  (via HTTP callback).  We decode the message into individual sensor
+//  values.
 
-//  Message looks like 0038028012709870:
-//  Message ID: 0038 (running sequence number, starting at 0)
-//  Temperature: 028.0 (degrees Celsius)
-//  Light Level: 127.0
-//  Acceleration: 987.0
+//  Message looks like "0038028012709870", which will be decoded as:
+//  - Message ID: 0038 (running sequence number, starting at 0)
+//  - Temperature: 028.0 (degrees Celsius)
+//  - Light Level: 127.0
+//  - Acceleration: 987.0
+//  Each field is 4 digits, scaled by 10 except Message ID.
 
-//  This function returns the sensor values as:
+//  This function returns the decoded sensor values in thethings.io format:
 //  [ { key: 'mid', value: 38 },
 //    { key: 'tmp', value: 28.0 },
 //    { key: 'lig', value: 127.0 },
 //    { key: 'acl', value: 987.0 } ]
 
-//  This should match the list of sensors in the microbit Setup Sigfox function call.
+//  List of expected sensor fields.  If you change the sensors in the 
+//  microbit "Setup Sigfox" function call, update this too.
 const sensors = ["tmp", "lig", "acl"];
 
-function main(params, callback){
-  console.log(JSON.stringify(params)); ////
+//  For convenience, insert the Message ID field before the sensor fields.
+//  This produces ["mid", "tmp", "lig", "acl"]
+const fields = ["mid"].concat(sensors);
+
+function main(params, callback) {
   //  Parse the 12-byte payload in the Sigfox message to get the sensor values.
-  //  Upon completion, callback will be passed an array of { key, value } sensor values.
-  if (!params) return callback(null, []);  //  Nothing to parse, quit.
-  let data = params.data;
+  //  Upon completion, callback will be passed an array of { key, value }
+  //  sensor values.
+  console.log(JSON.stringify(params));  //  Dump params to Developer Console.
+  if (!params) return callback(null, []);  //  If nothing to parse, quit.
+  let data = params.data;  //  We will parse "data", the Sigfox message.
   
-  //  Convert sensor values from text into an array of { key, values }.
-  const sensorValues = [];
-  const sensorsWithMessageID = ["mid"].concat(sensors);
-  sensorsWithMessageID.forEach(sensor => {
-    //  data looks like "0038028012709870". We decode the data 4 digits at a time.
+  //  Decode sensor values in "data" into an array of { key, values }.
+  const sensorValues = [];  //  Will contain array of { key, values }.
+  //  Repeat for each field "mid", "tmp", "lig", "acl"...
+  fields.forEach(sensor => {
+    //  data looks like "0038028012709870"
+    //  We decode the data 4 digits at a time.
     if (data.length < 4) { return; }  //  If no more digits to decode, we stop.
-    const scaledValueText = data.substr(0, 4);  //  Get the next 4 digits to decode.
-    const scaledValue = parseInt(scaledValueText, 10);  //  Convert the 4 digits into a number.
-    const value = scaledValue / 10.0;  //  Divide by 10.
-    sensorValues.push({ key: sensor, value: value });  //  Add the field to the decoded result.
+    const valueText = data.substr(0, 4);  //  Get the next 4 digits to decode.
+    
+    //  Convert the 4 digits into a number ("10" means decimal).
+    let value = parseInt(valueText, 10);
+    if (sensor !== "mid") {  //  If this is not the message ID...
+    	value = scaledValue / 10.0;  //  Divide the value by 10.
+    }
+    
+    //  Add the value to the decoded sensor values.
+    //  Looks like: { key: 'tmp', value: 28.0 }
+    sensorValues.push({ key: sensor, value: value });
     data = data.substr(4);  //  Continue decoding the next 4 digits.
   });
-  //  Return the sensor value array to thethings.io.
+  //  Return the decoded sensor values to thethings.io. Looks like:
+  //  { key: 'mid', value: 38 }, { key: 'tmp', value: 28.0 }, ...
   callback(null, sensorValues);
-
-  /*
-  const sensorValues = {};
-  const custom = params.custom;
-  if (custom) {
-    //  If custom sensor values are produced by the Sigfox Backend message parser,
-    //  return the custom values. So params.custom.tmp becomes tmp.
-    Object.assign(sensorValues, custom);
-  }
-  if (data && data.length >= 2 && data.length <= 16) {  //  Ignore Structured Messages.
-    //  If payload data contains one or more bytes, return each byte as a sensor value
-    //  data looks like "0038028012709870".  We break into fields of 4 digits each, divide hmd, lig, acl by 10:
-    //  mid=38, tmp=28.0, lig=127.0, acl=987.0.
-    if (data.length >= 4) sensorValues.mid = parseInt(data.substr(0, 4), 10);
-    if (data.length >= 8) sensorValues.tmp = parseInt(data.substr(4, 4), 10) / 10.0;
-    if (data.length >= 12) sensorValues.lig = parseInt(data.substr(8, 4), 10) / 10.0;
-    if (data.length >= 16) sensorValues.acl = parseInt(data.substr(12, 4), 10) / 10.0;
-  }
-  */
 }
